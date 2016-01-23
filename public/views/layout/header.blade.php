773a3c033eccad34765db633b6d05feb1b4abffe
Pritam Sharma <pritam751@gmail.com>
2016-01-23 17:41:46 +0530

    Adding logo and banner image

--- public/views/layout/header.blade.php
+++ public/views/layout/header.blade.php
@@ -4,15 +4,19 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header mobile-center">
-            <a class="navbar-brand" href="#"><img src="assets/home/images/gym-logo.png" alt="Yoga logo"> </a>
-
+            <a class="navbar-brand" href="{{url('/')}}">
+                <img src="assets/home/images/logo.png" alt="Sportofitt" /> 
+            </a>
+            <span class="click-to-call"  style="margin-top: 20px;" ><a href="{{url('/select')}}" target="_blank">Become A Vendor</a></span>
         </div>
+        
 
         <div class="click-to-call hidden-xs">
+            <a href="mailto:{{$email}}"><span class="phone-icon"><i class="fa fa-mail-forward"></i></span><span> {{$email}} </span></a>
             <a href="tel:{{$cell}}" target="_blank">
                 <span class="phone-icon"><i class="fa fa-phone"></i></span> <span>{{$cell}}</span>
             </a>
-            <a href="mailto:{{$email}}"><span>{{$email}}</span></a>
+            
         </div>
 
     </div>
