a8e5e0ada27372b61e75db4d6794d9db9fb9645d
Pritam Sharma <pritam751@gmail.com>
2016-01-26 21:39:48 +0530

    Changing the copyright messages at footer

--- public/views/layout/footer.blade.php
+++ public/views/layout/footer.blade.php
@@ -1,24 +1,24 @@
 <footer>
-    <div class="social-icons">
+<!--    <div class="social-icons">
         <a href="https://twitter.com/surjithctly" target="_blank"> <i class="fa fa-twitter"></i> </a>
         <a href="https://facebook.com/web3canvas" target="_blank"> <i class="fa fa-facebook"></i> </a>
         <a href="https://linkedin.com/" target="_blank"> <i class="fa fa-linkedin"></i> </a>
-    </div>
-    <p> <small class="text-muted">Copyright © 2015. All rights reserved. Made in India</small></p>
+    </div>-->
+    <p> <small class="text-muted">Copyright © 2016 Aquanta Consulting Services LLP</small></p>
 </footer>
-<a href="#top" class="back_to_top"><img src="assets/home/images/back_to_top.png" alt="back to top"></a>
+<a href="#top" class="back_to_top"><img src="{{url('assets/home/images/back_to_top.png')}}" alt="back to top"></a>
 
 <!-- 
  Javascripts : Nerd Stuff :)
  ====================================== -->
 <!-- jQuery Library -->
-<script src="assets/home/js/jquery.min.js"></script>
+<script src="{{url('assets/home/js/jquery.min.js')}}"></script>
 <!-- Bootstrap JS -->
-<script src="assets/home/js/bootstrap.min.js"></script>
+<script src="{{url('assets/home/js/bootstrap.min.js')}}"></script>
 <!-- 3rd party Plugins -->
-<script src="assets/home/js/plugins/validate.js"></script>
-<script src="assets/home/js/plugins/nicescroll.js"></script>
+<script src="{{url('assets/home/js/plugins/validate.js')}}"></script>
+<script src="{{url('assets/home/js/plugins/nicescroll.js')}}"></script>
 <!-- JS Includes -->
-<script src="assets/home/js/includes/contact_form.js"></script>
+<script src="{{url('assets/home/js/includes/contact_form.js')}}"></script>
 <!-- Main Script -->
-<script src="assets/home/js/main.js"></script>
+<script src="{{url('assets/home/js/main.js')}}"></script>
