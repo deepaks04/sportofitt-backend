c6b322abb1012b624e38339a4b3fb3571dd2dac5
Pritam Sharma <pritam751@gmail.com>
2016-02-21 15:21:09 +0530

    Adding numerical validation for pincode

--- app/Http/Requests/CustomerProfileUpdateRequest.php
+++ app/Http/Requests/CustomerProfileUpdateRequest.php
@@ -30,7 +30,7 @@ class CustomerProfileUpdateRequest extends Request
             'profile_picture' => 'mimes:jpeg,png,jpg',
             'birthdate' => 'date_format:d-m-Y',
             'area_id' => 'required',
-            'pincode' => 'required|min:6|max:6'
+            'pincode' => 'required|min:6|max:6|numeric'
         ];
     }
     
