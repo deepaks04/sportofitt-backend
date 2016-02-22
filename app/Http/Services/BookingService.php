9100c6a76ebb295126e446003c356ff3cf667ad2
pritam <pritam751@gmail.com>
2016-02-22 15:06:45 +0530

    For showing users booking by userid ?

--- app/Http/Services/BookingService.php
+++ app/Http/Services/BookingService.php
@@ -1,135 +1,35 @@
-<?php
+<?php namespace App\Http\Services;
 
-namespace App\Http\Services;
-
-use URL;
-use File;
-use App\City;
-use App\User;
-use App\Customer;
+use App\SessionBooking;
 use App\Http\Services\BaseService;
-use App\Http\Helpers\APIResponse;
-use App\Http\Helpers\FileHelper;
 
-class DashboardService extends BaseService
-{
+class BookingService extends BaseService {
 
     /**
-     * Getting users profile inforamtion of authenticated user based on
-     * the token
-     * 
-     * @return array
+     *
+     * @var mixed (NULL| App\SessionBooking)
      */
-    public function getUserProfile()
+    private $sessionBooking = null;
+
+    public function __construct()
     {
-        $areaDetails = $cityDetails = $customer = array();
-        try {
-            $user = $this->getAuthenticatedUser();
-            $city = City::select('id', 'name')->where('name', '=', 'pune')->first();
-            if (!empty($city) && $city->id > 0) {
-                $areas = $city->areas()->select('id', 'name', 'city_id')->get();
-                $cityDetails = $city->toArray();
-                $areaDetails = $areas->toArray();
-            }
-
-            if ($user->profile_picture != null) {
-                $vendorUploadPath = URL::asset(env('CUSTOMER_FILE_UPLOAD'));
-                $vendorOwnDirecory = $vendorUploadPath . "/" . sha1($user->id) . "/" . "profile_image/";
-                $user['profile_picture'] = $vendorOwnDirecory . "thumb_267X267_" . $user->profile_picture;
-            }
-
-            $userMeta = $user->customer()->select('pincode', 'area_id', 'phone_no', 'gender', 'birthdate')->get();
-            if (!empty($userMeta)) {
-                $customer = $userMeta->toArray();
-                if($customer && $customer[0]['birthdate']) {
-                    $customer[0]['birthdate'] = date("d-m-Y",strtotime($customer[0]['birthdate']));
-                }
-            }
-
-            return [
-                'user' => $user->toArray(),
-                'cities' => $cityDetails,
-                'areas' => $areaDetails,
-                'customer' => $customer
-            ];
-        } catch (Exception $exception) {
-            APIResponse::handelException($exception);
-        }
-
-        return null;
+        $this->sessionBooking = new SessionBooking();
     }
 
     /**
-     * updating user profile information of authenticated user based on token
+     * Get user bookings according to user 
      * 
-     * @param array $data
-     * @return array
+     * @return mixed (NULL | App\SessionBooking)
+     * @throws Exception
      */
-    public function updateProfileInformation(array $data)
+    public function getUsersBooking()
     {
         try {
             $user = $this->getAuthenticatedUser();
-            $user->fname = !empty($data['fname']) ? $data['fname'] : $user->fname;
-            $user->lname = !empty($data['lname']) ? $data['lname'] : $user->lname;
-
-            if (!empty($data['profile_picture'])) {
-                $vendorUploadPath = public_path() . env('CUSTOMER_FILE_UPLOAD');
-                $vendorOwnDirecory = $vendorUploadPath . sha1($user->id);
-                $vendorImageUploadPath = $vendorOwnDirecory . "/" . "profile_image";
-
-                /* Create Upload Directory If Not Exists */
-                if (!file_exists($vendorImageUploadPath)) {
-                    File::makeDirectory($vendorImageUploadPath, $mode = 0777, true, true);
-                    chmod($vendorOwnDirecory, 0777);
-                    chmod($vendorImageUploadPath, 0777);
-                }
-
-                $extension = $data['profile_picture']->getClientOriginalExtension();
-                $fileName = sha1($user->id . time()) . ".{$extension}";
-                $data['profile_picture']->move($vendorImageUploadPath, $fileName);
-                chmod($vendorImageUploadPath, 0777);
-
-                $fileHelper = new FileHelper($data['profile_picture']);
-                $fileHelper->sourceFilename = $fileName;
-                $fileHelper->sourceFilepath = $vendorImageUploadPath . "/";
-                $fileHelper->destinationPath = $vendorImageUploadPath . "/";
-                $fileHelper->resizeImage('user', true);
-
-                $user->profile_picture = $fileName;
-            }
-
-            $user->save();
-            $this->addOrUpdateCustomerInformation($user, $data);
-
-            return $this->getUserProfile();
+            return $this->sessionBooking->getUsersBooking($user->id, $this->offset, $this->limit);
         } catch (Exception $exception) {
-            APIResponse::handelException($exception);
+            throw new Exception($exception->getMessage(), $exception->getCode(), $exception);
         }
-    }
-
-    /**
-     * If it is existing user update customer user information else add new 
-     * entry for the user with the provided data as customer
-     * 
-     * @param User $user
-     * @param array $data
-     */
-    private function addOrUpdateCustomerInformation(User $user, array $data)
-    {
-        $customer = $user->customer()->first();
-        
-        if (empty($customer)) {
-            $customer = new Customer();
-        }
-        
-        $customer->pincode = !empty($data['pincode']) ? $data['pincode'] : NULL;
-        $customer->area_id = !empty($data['area_id']) ? $data['area_id'] : NULL;
-        $customer->phone_no = !empty($data['phoneno']) ? $data['phoneno'] : NULL;
-        $customer->gender = !empty($data['gender']) ? $data['gender'] : NULL;
-        $customer->birthdate = !empty($data['birthdate']) ? date("Y-m-d", strtotime($data['birthdate'])) : NULL;        
-        $customer->user_id = $user->id;
-        
-        $customer->save();
     }
 
 }
