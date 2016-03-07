7079f6355ddbd0d3718af3844cc702a5c1a2341c
Pritam Sharma <pritam751@gmail.com>
2016-03-07 12:52:30 +0530

    Adding bootstrap style sheet

--- public/views/messages/index.blade.php
+++ public/views/messages/index.blade.php
@@ -1,4 +1,4 @@
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <form name="savemessage" action="savemessages" method="POST" class="form-horizontal" style="margin: 5px 5px 15px 10px;">
     @foreach($messages as $key => $message) 
     @if(is_array($message))
