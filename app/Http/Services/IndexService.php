f9ae08aa4c6b06afcb32140290fcadca11e43c75
Pritam Sharma <pritam751@gmail.com>
2016-03-16 00:02:18 +0530

    Showing all necessary information in vendor array including facility and user details and images

--- app/Http/Services/IndexService.php
+++ app/Http/Services/IndexService.php
@@ -71,8 +71,8 @@ class IndexService extends BaseService
         $vendor = $vendorObject->getVendorDetailsById($vendor_id);
         if ($vendor) {
             $result['vendor'] = $vendor;
-            $result['user'] = $vendor->user()->select('fname', 'lname', 'username', 'profile_picture')->get();
-            $result['facilities'] = $vendor->facility()
+            $result['vendor']['user'] = $vendor->user()->select('fname', 'lname', 'username', 'profile_picture')->get();
+            $result['vendor']['facilities'] = $vendor->facility()
                     ->select('available_facilities.*', 'sub_categories.name AS subCategoryName', 'sub_categories.slug AS subCategorySlug', 'root_categories.name AS rootCategoryName', 'root_categories.slug AS rootCategorySlug')
                     ->join('sub_categories', 'available_facilities.sub_category_id', '=', 'sub_categories.id')
                     ->join('root_categories', 'available_facilities.root_category_id', '=', 'root_categories.id')
