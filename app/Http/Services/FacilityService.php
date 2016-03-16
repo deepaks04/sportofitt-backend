12381e7e6f449159a71b89cba8a562ec1b392454
Pritam Sharma <pritam751@gmail.com>
2016-03-16 12:08:50 +0530

    Getting all facility information in vendor details API

--- app/Http/Services/FacilityService.php
+++ app/Http/Services/FacilityService.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Services;
 
-use App\Vendor;
-use App\OpeningHour;
 use App\AvailableFacility;
 use App\Http\Services\BaseService;
 
 class FacilityService extends BaseService
 {
 
+    /**
+     * Get opening hours, sessions and packages information about the facility
+     * according to the facility id 
+     * 
+     * @param integer $facilityId
+     * @return array
+     * @throws Exception
+     */
     public function getSessionsAndPackages($facilityId)
     {
         try {
@@ -17,7 +23,7 @@ class FacilityService extends BaseService
             $facility->openingHours = $facility->getOpenigHoursOfFacility($facilityId);
             $facility->packages = $facility->getFacilityPackages($facilityId);
             $facility->sessions = $facility->getFacilitySessions($facilityId);
-            
+
             return $facility;
         } catch (Exception $ex) {
             throw new Exception($ex);
--- app/Http/Services/IndexService.php
+++ app/Http/Services/IndexService.php
@@ -5,6 +5,7 @@ namespace App\Http\Services;
 use App\Vendor;
 use App\AvailableFacility;
 use App\Http\Services\BaseService;
+use App\Http\Services\FacilityService;
 
 class IndexService extends BaseService
 {
@@ -31,16 +32,16 @@ class IndexService extends BaseService
     public function getVendors(array $requestData)
     {
         try {
-            $lat =  (!empty($requestData['lat']))?$requestData['lat']:null;
-            $long =  (!empty($requestData['long']))?$requestData['long']:null;
-            $areaId =  (!empty($requestData['area_id']))?$requestData['area_id']:null;
-            $category =  (!empty($requestData['category']))?$requestData['category']:null;
+            $lat = (!empty($requestData['lat'])) ? $requestData['lat'] : null;
+            $long = (!empty($requestData['long'])) ? $requestData['long'] : null;
+            $areaId = (!empty($requestData['area_id'])) ? $requestData['area_id'] : null;
+            $category = (!empty($requestData['category'])) ? $requestData['category'] : null;
 
             $vendor = new Vendor();
             $vendors = $vendor->searchVendors($lat, $long, $areaId, $category);
-            if($vendors) {
-                foreach($vendors as $vendor) {
-                    $vendor->type = (1 == $vendor->type)?'Venue':'Coaching';
+            if ($vendors) {
+                foreach ($vendors as $vendor) {
+                    $vendor->type = (1 == $vendor->type) ? 'Venue' : 'Coaching';
                     $vendor->gallery = $vendor->getVendorImages();
                     $vendor->features = $vendor->getVendorsFeatures();
                     $vendor->color = '';
@@ -48,17 +49,16 @@ class IndexService extends BaseService
                     $vendor->rating = 0;
                     $vendor->type_icon = '';
                     $vendor->url = '';
-                }                
+                }
             }
-            
+
             return $vendors;
-            
         } catch (Exception $ex) {
             throw new Exception($ex);
         }
     }
-    
-/**
+
+    /**
      * Getting details of the search result
      * 
      * @param integer $vendor_id
@@ -72,15 +72,40 @@ class IndexService extends BaseService
         if ($vendor) {
             $result['vendor'] = $vendor;
             $result['vendor']['user'] = $vendor->user()->select('fname', 'lname', 'username', 'profile_picture')->first();
-            $result['vendor']['facilities'] = $vendor->facility()
-                    ->select('available_facilities.id', 'available_facilities.name')
-                    ->join('sub_categories', 'available_facilities.sub_category_id', '=', 'sub_categories.id')
-                    ->join('root_categories', 'available_facilities.root_category_id', '=', 'root_categories.id')
-                    ->get();
-            $vendor->images = $vendor->getVendorImages();
+            $result['vendor']['facility'] = $this->getVendorsFacilities($vendor);
+            $result['vendor']['images'] = $vendor->getVendorImages();
         }
 
         return $result;
+    }
+
+    /**
+     * Get extra information about the 
+     * 
+     * @param Collection AvailableFacility $vendorFaciilityArray
+     * @return array Facility information
+     */
+    private function getVendorsFacilities($vendor)
+    {
+        $facilities = $vendor->facility()
+                ->select('available_facilities.id', 'available_facilities.name')
+                ->join('sub_categories', 'available_facilities.sub_category_id', '=', 'sub_categories.id')
+                ->join('root_categories', 'available_facilities.root_category_id', '=', 'root_categories.id')
+                ->where('available_facilities.is_active', '=', \DB::raw(1))
+                ->get();
+
+        if (!empty($facilities) && count($facilities) > 0) {
+            foreach ($facilities as $key => $facility) {
+                try {
+                    $facilityService = new FacilityService();
+                    $facilities[$key] = $facilityService->getSessionsAndPackages($facility->id);
+                } catch (Exception $ex) {
+                    continue;
+                }
+            }
+        }
+
+        return $facilities;
     }
 
 }
