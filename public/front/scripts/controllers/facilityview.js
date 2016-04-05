299a01c7278e3e868325048d5a2ef87c94ca2488
eastern_maheshb <p.kadam@easternenterprise.com>
2016-04-05 17:22:45 +0530

    modal open

--- public/front/scripts/controllers/facilityview.js
+++ public/front/scripts/controllers/facilityview.js
@@ -8,63 +8,66 @@
  * Controller of the sportofittApp
  */
 angular.module('sportofittApp')
-  .controller('FacilityViewCtrl', function ($stateParams,$state,searchService,toastr,$scope,bookingService) {
-      var vm = this;
-      vm.facilityId = $stateParams.facilityId;
-      var draggableMarker = false;
+        .controller('FacilityViewCtrl', function ($stateParams, $state, searchService, toastr, $scope, bookingService) {
+            var vm = this;
+            vm.facilityId = $stateParams.facilityId;
+            var draggableMarker = false;
 
-      vm.days = {1:'Monday',2:'Thusday',3:'Wednesday',4:'Thursday',5:'Friday',6:'Saturday',7:'Sunday'};
+            vm.days = {1: 'Monday', 2: 'Thusday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'};
 
-      var mapStyles = [ {"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"on"},{"lightness":10}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":50}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]}, {featureType:'road.highway',elementType:'all',stylers:[{hue:'#dddbd7'},{saturation:-92},{lightness:60},{visibility:'on'}]}, {featureType:'landscape.natural',elementType:'all',stylers:[{hue:'#c8c6c3'},{saturation:-71},{lightness:-18},{visibility:'on'}]},  {featureType:'poi',elementType:'all',stylers:[{hue:'#d9d5cd'},{saturation:-70},{lightness:20},{visibility:'on'}]} ];
+            var mapStyles = [{"featureType": "road", "elementType": "labels", "stylers": [{"visibility": "simplified"}, {"lightness": 20}]}, {"featureType": "administrative.land_parcel", "elementType": "all", "stylers": [{"visibility": "off"}]}, {"featureType": "landscape.man_made", "elementType": "all", "stylers": [{"visibility": "on"}]}, {"featureType": "transit", "elementType": "all", "stylers": [{"saturation": -100}, {"visibility": "on"}, {"lightness": 10}]}, {"featureType": "road.local", "elementType": "all", "stylers": [{"visibility": "on"}]}, {"featureType": "road.local", "elementType": "all", "stylers": [{"visibility": "on"}]}, {"featureType": "road.highway", "elementType": "labels", "stylers": [{"visibility": "simplified"}]}, {"featureType": "poi", "elementType": "labels", "stylers": [{"visibility": "off"}]}, {"featureType": "road.arterial", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": 50}]}, {"featureType": "water", "elementType": "all", "stylers": [{"hue": "#a1cdfc"}, {"saturation": 30}, {"lightness": 49}]}, {"featureType": "road.highway", "elementType": "geometry", "stylers": [{"hue": "#f49935"}]}, {"featureType": "road.arterial", "elementType": "geometry", "stylers": [{"hue": "#fad959"}]}, {featureType: 'road.highway', elementType: 'all', stylers: [{hue: '#dddbd7'}, {saturation: -92}, {lightness: 60}, {visibility: 'on'}]}, {featureType: 'landscape.natural', elementType: 'all', stylers: [{hue: '#c8c6c3'}, {saturation: -71}, {lightness: -18}, {visibility: 'on'}]}, {featureType: 'poi', elementType: 'all', stylers: [{hue: '#d9d5cd'}, {saturation: -70}, {lightness: 20}, {visibility: 'on'}]}];
 
-      vm.init = function(){
-          searchService.getFacilityById(vm.facilityId).then(function(response){
-              vm.facility = response.data.data;
-              console.log( vm.facility);
-              //simpleMap(vm.facility.latitude, vm.facility.longitude, draggableMarker);
-          }).catch(function(response){
-              toastr.error(response);
-          })
-      }
+            vm.init = function () {
+                searchService.getFacilityById(vm.facilityId).then(function (response) {
+                    vm.facility = response.data.data;
+                    //simpleMap(vm.facility.latitude, vm.facility.longitude, draggableMarker);
+                }).catch(function (response) {
+                    toastr.error(response);
+                })
+            }
 
-      function simpleMap(_latitude, _longitude, draggableMarker){
-          var mapCenter = new google.maps.LatLng(_latitude, _longitude);
-          var mapOptions = {
-              zoom: 14,
-              center: mapCenter,
-              disableDefaultUI: true,
-              scrollwheel: false,
-              styles: mapStyles,
-              panControl: false,
-              zoomControl: false,
-              draggable: true
-          };
-          var mapElement = document.getElementById('map-simple');
-          var map = new google.maps.Map(mapElement, mapOptions);
+            function simpleMap(_latitude, _longitude, draggableMarker) {
+                var mapCenter = new google.maps.LatLng(_latitude, _longitude);
+                var mapOptions = {
+                    zoom: 14,
+                    center: mapCenter,
+                    disableDefaultUI: true,
+                    scrollwheel: false,
+                    styles: mapStyles,
+                    panControl: false,
+                    zoomControl: false,
+                    draggable: true
+                };
+                var mapElement = document.getElementById('map-simple');
+                var map = new google.maps.Map(mapElement, mapOptions);
 
-          // Google map marker content -----------------------------------------------------------------------------------
+                // Google map marker content -----------------------------------------------------------------------------------
 
-          var markerContent = document.createElement('DIV');
-          markerContent.innerHTML =
-              '<div class="map-marker">' +
-              '<div class="icon"></div>' +
-              '</div>';
+                var markerContent = document.createElement('DIV');
+                markerContent.innerHTML =
+                        '<div class="map-marker">' +
+                        '<div class="icon"></div>' +
+                        '</div>';
 
-          // Create marker on the map ------------------------------------------------------------------------------------
+                // Create marker on the map ------------------------------------------------------------------------------------
 
-          var marker = new RichMarker({
-              //position: mapCenter,
-              position: new google.maps.LatLng( _latitude, _longitude ),
-              map: map,
-              draggable: draggableMarker,
-              content: markerContent,
-              flat: true
-          });
+                var marker = new RichMarker({
+                    //position: mapCenter,
+                    position: new google.maps.LatLng(_latitude, _longitude),
+                    map: map,
+                    draggable: draggableMarker,
+                    content: markerContent,
+                    flat: true
+                });
 
-          marker.content.className = 'marker-loaded';
-      }
-
+                marker.content.className = 'marker-loaded';
+            }
 
 
-      vm.init();
-  });
+            vm.addLocalBooking = function (booking) {
+                bookingService.saveLocalBooking(booking);
+                $state.go('app.orderconformation', {});
+            }
+
+            vm.init();
+        });
