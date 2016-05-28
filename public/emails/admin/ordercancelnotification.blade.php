be5a0fdfafa85704c5464d271576fe4407e4dcb4
Pritam Sharma <pritam751@gmail.com>
2016-05-28 17:47:59 +0530

    Addin templates for the mails of order

--- public/emails/admin/ordercancelnotification.blade.php
+++ public/emails/admin/ordercancelnotification.blade.php
@@ -4,25 +4,12 @@
         <meta charset="utf-8">
     </head>
     <body>
-        <p> Following user has been registered with the system.</p>
-        <div>
-            <table>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                </tr>
-                <tr>
-                    <td>{{$fname}}</td>
-                    <td>{{$lname}}</td>
-                    <td>{{$email}}</td>
-                </tr>
-            </table>
-        </div>
+        <p> Hello There, </p>
+        <p>{{$fname}} {{$lname}} has cancelled his/her booking {{$orderId}}.</p>
+        <p><i>Note: Cancellation &amp; refund policy are applicable as mentioned by Venue providers.</i></p>
         <p>
             Stay Fit,<br> The team at Sportofit
-        </p>
+        </p>        
         <p>Thank you</p>
-
     </body>
 </html>
--- public/emails/ordercancel.blade.php
+++ public/emails/ordercancel.blade.php
@@ -4,19 +4,12 @@
         <meta charset="utf-8">
     </head>
     <body>
-
-
-        <p>Welcome to Sportofitt! {{$fname}} {{$lname}}, You are almost there.
-            Just click the link below to confirm your email address.</p>
-        <p></p>
-        <a href='{{ URL::to("front/#/confirmation/$remember_token") }}'> <span
-                style="color: blue; font-weight: 900">Click on this, to confirm your
-                account!!</span>
-        </a>
-    <p>
-        Stay Fit,<br> The team at Sportofitt
-    </p>
-    <p>Thank you</p>
-
-</body>
+        <p> Dear {{$fname}}, </p>
+        <p>Your booking {{$orderId}} has been cancelled. Kindly show this message or SMS at the reception of the venue for any further queries.</p>
+        <p><i>Note: Cancellation &amp; refund policy are applicable as mentioned by Venue providers.</i></p>
+        <p>
+            Stay Fit,<br> The team at Sportofit
+        </p>        
+        <p>Thank you</p>
+    </body>
 </html>
