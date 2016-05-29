a167e671157dbaecf67fd6ca33c3ff476fc31dde
Pritam Sharma <pritam751@gmail.com>
2016-05-30 01:01:08 +0530

    changing users profile image crop dimensions

--- app/Http/Helpers/FileHelper.php
+++ app/Http/Helpers/FileHelper.php
@@ -65,7 +65,7 @@ class FileHelper
      * 
      * @var dimensions for user profile images. 
      */
-    public $user = array('small' => array('267X267'));
+    public $user = array('small' => array('262X196'));
     public $ticker = array('small' => array('555X415'));
     protected static $userDirs = array('image', 'audio', 'video', 'thumbnail');
 
